114d9ab872fb027774e27f32e62c878d5d4f2f38
billtsai1518 <billtsai1518@gmail.com>
2021-03-18 10:30:26 +0800

    Update comments

--- line-notify-message.py
+++ line-notify-message.py
@@ -7,15 +7,16 @@ def lineNotifyMessage(token, msg):
         "Authorization": "Bearer " + token, 
         "Content-Type" : "application/x-www-form-urlencoded"
     }
-
-    payload = {'message': msg }
+    payload = {'message': msg } # Ref: https://notify-bot.line.me/doc/en/
+    
     r = requests.post("https://notify-api.line.me/api/notify", headers = headers, params = payload)
-    return r.status_code
+    return r.status_code # status_code returns 200, 400, 401, etc.
 
 
 if __name__ == "__main__":
     wd = datetime.today().isoweekday()
     if 1 <= wd and wd <= 5: # Means "if today is between Monday and Friday"
+        # LINE_NOTIFY_TOKEN and LINE_NOTIFY_MESSAGE are both Heroku Config Vars
         token = os.environ['LINE_NOTIFY_TOKEN']
         message = os.environ['LINE_NOTIFY_MESSAGE']
         lineNotifyMessage(token, message)
